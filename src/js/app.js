dbe0fbb74e28f20f43176be08a05b36ce30485d5
Dmitry <79225555735@ya.ru>
2017-01-07 14:35:13 +0500

    add min width shadow

--- src/js/app.js
+++ src/js/app.js
@@ -69,6 +69,72 @@ $('.gallery').each(function(idx, gallery)
 {
   if($(gallery).find('.gallery-item').length > 9)
   {
-    console.log('Надо свернуть');
+    console.log('Need fold');
   }
 });
+
+/*
+* Min Width Shadow
+*/
+
+(function ( $ ) {
+  $.fn.minWidthShadow = function()
+  {
+    function shadowInTable(tableWrapper){
+      var wrapper = tableWrapper,
+          wrapperWidth = wrapper.width(),
+          table = wrapper.children(),
+          tableWidth = table.width(),
+          leftShadow = wrapper.siblings('.shdwtab__shad_lt'),
+          rightShadow = wrapper.siblings('.shdwtab__shad_rt'),
+          scrollLeft = wrapper.scrollLeft(),
+          scrollRight = tableWidth - scrollLeft - wrapperWidth,
+          visibleClass = 'shdwtab__shad_visible';
+
+      // Left shadow
+      if(scrollLeft > 5){
+        leftShadow.addClass(visibleClass)
+        leftShadow.css({'opacity' : scrollLeft / 100})
+      }else{
+        leftShadow.removeClass(visibleClass)
+        leftShadow.css({'opacity' : ''})
+      }
+
+      // Right shadow
+      if(scrollRight > 5){
+        rightShadow.addClass(visibleClass)
+        rightShadow.css({'opacity' : scrollRight / 100})
+      }else{
+        rightShadow.removeClass(visibleClass)
+        rightShadow.css({'opacity' : ''})
+      }
+    }
+
+    return this.each(function()
+    {
+      /* Template */
+      var shadowWrap =  '<div class="shdwtab">'
+                          + '<div class="shdwtab__shad shdwtab__shad_lt"></div>'
+                          + '<div class="shdwtab__shad shdwtab__shad_rt"></div>'
+                          + '<div class="shdwtab__wrap"></div>'
+                      + '</div>';
+      
+      /* Add template */
+      var newTable = $(shadowWrap).insertBefore($(this));
+      var newTableWrap = newTable.find('.shdwtab__wrap');
+
+      /* Wrap to template */
+      newTableWrap.append($(this));
+
+      /* Handlers */ 
+      newTableWrap.scroll(function() { shadowInTable($(this)); });
+      // TODO: this init one and global
+      // $(window).resize(function() { shadowInTable($(newTableWrap)); });  
+
+      /* Init */ 
+      shadowInTable(newTableWrap);
+    });
+  };
+}( jQuery ));
+
+$('table, .winguru').minWidthShadow();
