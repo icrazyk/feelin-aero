4f11c5412eeea4a62ce133175a66d6e0a3036cec
Dmitriy Karyakin <skycrazyk@gmail.com>
2018-08-06 19:23:38 +0500

    fix fly btn

--- wp-feelin-aero/header.php
+++ wp-feelin-aero/header.php
@@ -10,6 +10,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> -->
   <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
   <?php endif; ?>
